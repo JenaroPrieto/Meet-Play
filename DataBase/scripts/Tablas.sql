ba27697de39bfd8d0c72f603464c524532fda9a1
Jenaro Prieto <jenaro.prieto@uc.cl>
2025-10-10 15:30:38 -0300

    edit de definicion tablas

--- DataBase/scripts/Tablas.sql
+++ DataBase/scripts/Tablas.sql
@@ -1,20 +1,19 @@
-
 -- 1. USUARIO
 CREATE TABLE Usuario (
-    id_usuario SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
-    contrasena_hash VARCHAR(255) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
     fecha_registro TIMESTAMP DEFAULT NOW(),
     foto_perfil VARCHAR(255),
-    ubicacion VARCHAR(255),
+    direccion VARCHAR(255),
     latitud DECIMAL(10,7),
     longitud DECIMAL(10,7)
 );
 
 -- 2. CANCHA
 CREATE TABLE Cancha (
-    id_cancha SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     direccion VARCHAR(255),
     comuna VARCHAR(100),
@@ -27,18 +26,20 @@ CREATE TABLE Cancha (
 
 -- 3. DEPORTE
 CREATE TABLE Deporte (
-    id_deporte SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     max_participantes INT
 );
 
+-- 4.0 TYPE estado partido
+CREATE TYPE ESTADO_PARTIDO AS ENUM ('abierto', 'cerrado');
+
 -- 4. PARTIDO
 CREATE TABLE Partido (
-    id_partido SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     titulo VARCHAR(100),
-    fecha DATE,
-    hora TIME,
-    estado VARCHAR(20) DEFAULT 'abierto',
+    fecha TIMESTAMP,
+    estado ESTADO DEFAULT 'abierto',
     id_creador INT,
     id_cancha INT,
     id_deporte INT,
@@ -52,9 +53,8 @@ CREATE TABLE Partido (
 
 -- 5. CHAT
 CREATE TABLE Chat (
-    id_chat SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     nombre VARCHAR(100),
-    tipo VARCHAR(50),
     id_partido INT,
     FOREIGN KEY (id_partido) REFERENCES Partido(id_partido)
         ON DELETE CASCADE
@@ -62,9 +62,8 @@ CREATE TABLE Chat (
 
 -- 6. MENSAJE
 CREATE TABLE Mensaje (
-    id_mensaje SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     fecha_envio TIMESTAMP DEFAULT NOW(),
-    estado VARCHAR(50),
     mensaje TEXT NOT NULL,
     id_usuario INT,
     id_chat INT,
